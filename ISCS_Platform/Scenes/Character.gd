bff11861ebe11d14c87b2d80da5be0907ec15fd4
NxKuma <neilisraelko01@gmail.com>
2024-08-31 20:04:49 +0800

    SetUp for Movement Refinement
    Raycast is moving but animation does not compute

--- ISCS_Platform/Scenes/Character.gd
+++ ISCS_Platform/Scenes/Character.gd
@@ -3,9 +3,14 @@ extends Node2D
 @onready var tile_map = $"../TileMap"
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
+@onready var ray_2d = $RayCast2D
 
-var is_moving = false
-var tile = Vector2(0,0)
+var can_animate: bool = true
+var is_moving: bool = false
+var on_water: bool = false
+var on_mud: bool = false
+var tile: Vector2 = Vector2(0,0)
+
 # Called when the node enters the scene tree for the first time.
 
 func _physics_process(delta):
@@ -21,19 +26,45 @@ func _process(delta):
 	if is_moving:
 		return
 	
-	
 	if Input.is_action_pressed("left"):
 		move(Vector2.LEFT)
-		animation_player.play("LeftHop")
+		animate(Vector2.LEFT)
 	elif Input.is_action_pressed("right"):
 		move(Vector2.RIGHT)
-		animation_player.play("RightHop")
+		animate(Vector2.RIGHT)
 	elif Input.is_action_pressed("up"):
 		move(Vector2.UP)
-		animation_player.play("UpHop")
+		animate(Vector2.UP)
 	elif Input.is_action_pressed("down"):
 		move(Vector2.DOWN)
-		animation_player.play("DownHop")
+		animate(Vector2.DOWN)
+	
+
+func animate(direction: Vector2):
+	if !can_animate:
+		return
+	
+	if on_water and !on_mud:
+		if direction == Vector2.LEFT:
+			animation_player.play("LeftSwim")
+		if direction == Vector2.UP:
+			animation_player.play("UpSwim")
+		if direction == Vector2.RIGHT:
+			animation_player.play("RightSwim")
+		if direction == Vector2.DOWN:
+			animation_player.play("DownSwim")
+	elif !on_water and on_mud:
+		animation_player.play("MudSlide")
+	else:
+		if direction == Vector2.LEFT:
+			animation_player.play("LeftHop")
+		if direction == Vector2.UP:
+			animation_player.play("UpHop")
+		if direction == Vector2.RIGHT:
+			animation_player.play("RightHop")
+		if direction == Vector2.DOWN:
+			animation_player.play("DownHop")
+		
 
 func move(direction: Vector2):
 	# Get current tile Vector2
@@ -46,12 +77,21 @@ func move(direction: Vector2):
 	# Get custom data layer 
 	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
 	
+	ray_2d.target_position = direction * 16
+	ray_2d.force_raycast_update()
+	if ray_2d.is_colliding():
+		can_animate = false
+		return
+	else:
+		can_animate = true
+	
 	for index in tile_map.get_layers_count():
 		tile_map.get_cell_tile_data(index, target_tile)
 		if !tile_data is TileData:
 			return
 		elif tile_data.get_custom_data("walkable") == false:
 			return
+			
 	
 	tile = tile_map.map_to_local(target_tile)
 	
