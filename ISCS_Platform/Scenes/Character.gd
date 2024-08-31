644e7237e302e52178528e556a113a1214de6fe1
NxKuma <neilisraelko01@gmail.com>
2024-08-31 09:33:11 +0800

    Fixed Character Movement

--- ISCS_Platform/Scenes/Character.gd
+++ ISCS_Platform/Scenes/Character.gd
@@ -2,25 +2,38 @@ extends Node2D
 
 @onready var tile_map = $"../TileMap"
 @onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
 
-#var is_moving = false
+var is_moving = false
+var tile = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _physics_process(delta):
+	if is_moving == false:
+		return
+	if animation_player.is_playing() == false:
+		is_moving = false
+	global_position = global_position.move_toward(tile, 0.6)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#if is_moving:
-		#return
-		
+	if is_moving:
+		return
+	
+	
 	if Input.is_action_pressed("left"):
 		move(Vector2.LEFT)
+		animation_player.play("LeftHop")
 	elif Input.is_action_pressed("right"):
 		move(Vector2.RIGHT)
+		animation_player.play("RightHop")
 	elif Input.is_action_pressed("up"):
 		move(Vector2.UP)
+		animation_player.play("UpHop")
 	elif Input.is_action_pressed("down"):
 		move(Vector2.DOWN)
+		animation_player.play("DownHop")
 
 func move(direction: Vector2):
 	# Get current tile Vector2
@@ -33,9 +46,15 @@ func move(direction: Vector2):
 	# Get custom data layer 
 	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
 	
-	#if tile_data.get_costum_data("walkable") == false:
-		#return
-		
-	#Move Player
-	#is_moving = true
-	global_position = tile_map.map_to_local(target_tile)
+	for index in tile_map.get_layers_count():
+		tile_map.get_cell_tile_data(index, target_tile)
+		if !tile_data is TileData:
+			return
+		elif tile_data.get_custom_data("walkable") == false:
+			return
+	
+	tile = tile_map.map_to_local(target_tile)
+	
+	
+	#global_position = tile_map.map_to_local(target_tile)
+	is_moving = true
