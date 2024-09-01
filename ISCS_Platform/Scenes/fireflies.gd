3dad7c3b5c1221c523af41ea9a965dd5995f0218
EnnaColada <enna.veloso@student.ateneo.edu>
2024-09-02 06:38:42 +0800

    Added Collision

--- ISCS_Platform/Scenes/fireflies.gd
+++ ISCS_Platform/Scenes/fireflies.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var tile_map: TileMap = $"../TileMap"
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var ray_2d: RayCast2D = $RayCast2D
 
 var rng: int = 0
 var tile: Vector2 = Vector2(0,0) 
@@ -14,22 +15,22 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	animation_player.play("idle")
+	rng = randi_range(0,3)
 
 func _physics_process(delta: float) -> void:
 	if global_position != tile:
 		current_tile = tile_map.local_to_map(global_position)
-		global_position = global_position.move_toward(tile, 0.6)
-		
+		global_position = global_position.move_toward(tile, 1)
 
-func move(direction: Vector2):
+func move():
+	# Mapping randomized directions
+	var direction: Vector2 = Vector2.ZERO
 	match rng: 
-		0: direction.x += 1
-		1: direction.x -= 1
-		2: direction.y += 1
-		3: direction.y -= 1
+		0: direction = Vector2.RIGHT
+		1: direction = Vector2.LEFT
+		2: direction = Vector2.UP
+		3: direction = Vector2.DOWN
 		
-	# Get current tile Vector2
-	
 	# Get target tile Vector2	
 	var target_tile: Vector2i = Vector2i(
 		current_tile.x + direction.x,
@@ -38,6 +39,11 @@ func move(direction: Vector2):
 	# Get custom data layer 
 	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
 	
+	ray_2d.target_position = direction * 16
+	ray_2d.force_raycast_update()
+	if ray_2d.is_colliding():
+		return
+	
 	for index in tile_map.get_layers_count():
 		tile_map.get_cell_tile_data(index, target_tile)
 		if !tile_data is TileData:
@@ -45,9 +51,8 @@ func move(direction: Vector2):
 		elif tile_data.get_custom_data("walkable") == false:
 			return
 
+
 	tile = tile_map.map_to_local(target_tile)
 	
-	
 func _on_timer_timeout() -> void:
-	pass
-	
+	move()
