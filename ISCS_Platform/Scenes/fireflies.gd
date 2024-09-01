4614bbac93d1554b35ec5d8d022aa0213b31d456
EnnaColada <enna.veloso@student.ateneo.edu>
2024-09-02 03:52:34 +0800

    Added Firefly entities

---
+++ ISCS_Platform/Scenes/fireflies.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+@onready var tile_map: TileMap = $"../TileMap"
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var rng: int = 0
+var tile: Vector2 = Vector2(0,0) 
+var current_tile: Vector2i
+
+func _ready() -> void:
+	rng = randi_range(0,3)
+	current_tile = tile_map.local_to_map(global_position)
+
+func _process(delta: float) -> void:
+	animation_player.play("idle")
+
+func _physics_process(delta: float) -> void:
+	if global_position != tile:
+		current_tile = tile_map.local_to_map(global_position)
+		global_position = global_position.move_toward(tile, 0.6)
+		
+
+func move(direction: Vector2):
+	match rng: 
+		0: direction.x += 1
+		1: direction.x -= 1
+		2: direction.y += 1
+		3: direction.y -= 1
+		
+	# Get current tile Vector2
+	
+	# Get target tile Vector2	
+	var target_tile: Vector2i = Vector2i(
+		current_tile.x + direction.x,
+		current_tile.y + direction.y,
+	)
+	# Get custom data layer 
+	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
+	
+	for index in tile_map.get_layers_count():
+		tile_map.get_cell_tile_data(index, target_tile)
+		if !tile_data is TileData:
+			return
+		elif tile_data.get_custom_data("walkable") == false:
+			return
+
+	tile = tile_map.map_to_local(target_tile)
+	
+	
+func _on_timer_timeout() -> void:
+	pass
+	
