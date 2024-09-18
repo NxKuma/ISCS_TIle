3129bb4b80c8825f4cf1af118bcf8416be38ba29
NxKuma <neilisraelko01@gmail.com>
2024-09-18 22:04:26 +0800

    Credits ADded

--- ISCS_Platform/Scenes/Main.gd
+++ ISCS_Platform/Scenes/Main.gd
@@ -2,12 +2,17 @@ extends Node2D
 
 @onready var camera:Camera2D = $Character/Sprite2D/Camera2D
 @onready var text: RichTextLabel = $UI/Control/MarginContainer/RichTextLabel
+@onready var credits_marker: Marker2D = $Credits
+@onready var tile_map: TileMap = $TileMap
+@onready var character: Node2D = $Character
+@onready var credits: RichTextLabel = $UI/Control/MarginContainer/Credits
 
 var did_press: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	camera.zoom = Vector2(8,8)
+	#credits.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,3 +22,8 @@ func _process(delta):
 		
 	if did_press and camera.zoom != Vector2(5,5):
 		camera.zoom = camera.zoom.move_toward(Vector2(5,5),0.07)
+	
+	if character.global_position == credits_marker.global_position:
+		credits.show()
+	else:
+		credits.hide()
